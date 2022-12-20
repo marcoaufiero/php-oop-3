330c3d0279809d5f5da16e4835f200b831078798
marcoaufiero <marcoaufiero96@gmail.com>
2022-12-20 17:11:33 +0100

    trait "id" added

---
+++ traits/trait.php
@@ -0,0 +1,26 @@
+<?php 
+
+trait id{
+
+    public $character;
+    public $number;
+
+    public function __construct(
+        String $character,
+        Int $number
+        
+        ) {
+        $this->character = $character;
+        $this->number = $number;
+    }
+
+    public function getId($character,$number){
+        return "ID : $character$number"
+    }
+
+}
+
+
+
+
+?>
